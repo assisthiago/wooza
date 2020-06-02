f3c05d613f590fc4a82be1a1bef2936d7cc37d7b
Thiago Assis <thiago.medassis@gmail.com>
2020-06-02 20:58:54 -0300

    Configurando as urls do Plano

--- app/wooza/urls.py
+++ app/wooza/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('plans/', include('plans.urls')),
     path('admin/', admin.site.urls),
 ]
