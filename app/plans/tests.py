e5571c7dc009dfe36eeb6e21c855ba87f261810b
Thiago Assis <thiago.medassis@gmail.com>
2020-06-03 17:03:23 -0300

    Testando a busca utilizando um código de plano existente com o ddd

--- app/plans/tests.py
+++ app/plans/tests.py
@@ -572,3 +572,15 @@ class PlanSearchTestCase(TestCase):
         self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
         self.assertContains(response, '"data": []', status_code=404)
         self.assertContains(response, '"total": 0', status_code=404)
+
+    def test_valid_plan_code_with_ddd_filter(self):
+        querystring = 'ddds=[21]&plan_code=TimControle20gb200'
+        path = self.url + querystring
+
+        response = self.client.get(path, content_type=self.content_type)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+        self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
+        self.assertContains(response, '"plan_code": "TimControle20gb200"')
+        self.assertContains(response, '"total": 1')
