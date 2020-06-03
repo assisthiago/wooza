c3e9eba2d0c8aa9883952581e7752f8f14686b38
Thiago Assis <thiago.medassis@gmail.com>
2020-06-03 17:02:48 -0300

    Testando a busca utilizando um código de plano inexistente com o ddd

--- app/plans/tests.py
+++ app/plans/tests.py
@@ -561,3 +561,14 @@ class PlanSearchTestCase(TestCase):
         self.assertContains(response, '"plan_code": "OiPos10gb100"')
         self.assertContains(response, '"plan_code": "TimControle20gb200"')
         self.assertContains(response, '"total": 2')
+
+    def test_invalid_plan_code_with_ddd_filter(self):
+        querystring = 'ddds=[21]&plan_code=Orelhao21'
+        path = self.url + querystring
+
+        response = self.client.get(path, content_type=self.content_type)
+
+        self.assertEqual(response['content-type'], 'application/json')
+        self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
+        self.assertContains(response, '"data": []', status_code=404)
+        self.assertContains(response, '"total": 0', status_code=404)
