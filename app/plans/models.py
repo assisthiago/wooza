c485cccb2b40e0821abb869be2dea000f62c721e
Thiago Assis <thiago.medassis@gmail.com>
2020-06-02 23:03:08 -0300

    Acertando o modelo

--- app/plans/models.py
+++ app/plans/models.py
@@ -2,11 +2,11 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
-class Plan(models.Model):
-    id = models.IntegerField(primary_key=True)
+class Plans(models.Model):
+    id = models.AutoField(primary_key=True)
     plan_code = models.CharField(max_length=50)
     minutes = models.IntegerField()
-    internet = models.IntegerField()
+    internet = models.CharField(max_length=10)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     plan_type = models.CharField(max_length=8)
     operator = models.CharField(max_length=6)
